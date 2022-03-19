ec71316a9b615274bd71efa680bf1749218850e4
Dominik Melnarowicz <dominik.melnarowicz@netguru.com>
2022-03-19 13:38:42 +0100

    docker available

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+from main import get_app, get_tracer
+import os
+
+service_name = "my-shop-service"
+tracer = get_tracer(service_name, os.environ["JAEGER_HOST"])
+app = get_app(tracer, service_name)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
